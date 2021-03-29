e19e0e8ad4f4d515c9d4d17c6b9a1b96169c76c2
chengming Wang <wangc115@msu.edu>
2021-03-29 15:10:32 -0400

    fixed logic

--- scripts.js
+++ scripts.js
@@ -4,24 +4,25 @@ if (decidedToCancel) {
   let computerScore = 0
   for (let i = 0; i < 3; i++) {
     let userChoice = window.prompt('input rock, paper or scissors')
+    if (userChoice === null) {
+      break
+    }
     userChoice = userChoice.trim().toLowerCase()
     let computerChoice = ''
-    if (userChoice === 'rock' || 'paper' || 'scissors') {
-      const randomChoice = Math.random()
-      if (randomChoice < 0.34) {
-        computerChoice = 'rock'
-        window.alert('Computer is ' + computerChoice)
-      } else if (randomChoice >= 0.34 && randomChoice < 0.67) {
-        computerChoice = 'paper'
-        window.alert('Computer is ' + computerChoice)
-      } else if (randomChoice >= 0.67) {
-        computerChoice = 'scissors'
-        window.alert('Computer is ' + computerChoice)
-      }
+    const randomChoice = Math.random()
+    if (randomChoice < 0.34) {
+      computerChoice = 'rock'
+      window.alert('Computer is ' + computerChoice)
+    } else if (randomChoice >= 0.34 && randomChoice < 0.67) {
+      computerChoice = 'paper'
+      window.alert('Computer is ' + computerChoice)
+    } else if (randomChoice >= 0.67) {
+      computerChoice = 'scissors'
+      window.alert('Computer is ' + computerChoice)
     }
     if (userChoice === 'rock' && computerChoice === 'rock') {
-      playerScore += 1
-      computerScore += 1
+      playerScore += 0
+      computerScore += 0
       window.alert('Tie.')
     } else if (userChoice === 'rock' && computerChoice === 'paper') {
       playerScore += 0
@@ -36,29 +37,33 @@ if (decidedToCancel) {
       computerScore += 0
       window.alert('player win the match.')
     } else if (userChoice === 'paper' && computerChoice === 'paper') {
-      playerScore += 1
-      computerScore += 1
+      playerScore += 0
+      computerScore += 0
       window.alert('Tie')
     } else if (userChoice === 'paper' && computerChoice === 'scissors') {
       playerScore += 0
       computerScore += 1
       window.alert('Computer win the match.')
-    } else if (userChoice === 'scissors' && computerChoice === 'rock') {
+    } else if ((userChoice === 'scissors' || userChoice === 'scissor') && computerChoice === 'rock') {
       playerScore += 0
       computerScore += 1
       window.alert('Computer win the match.')
-    } else if (userChoice === 'scissors' && computerChoice === 'paper') {
+    } else if ((userChoice === 'scissors' || userChoice === 'scissor') && computerChoice === 'paper') {
       playerScore += 1
       computerScore += 0
       window.alert('player win the match.')
-    } else if (userChoice === 'scissors' && computerChoice === 'scissors') {
-      playerScore += 1
-      computerScore += 1
+    } else if ((userChoice === 'scissors' || userChoice === 'scissor') && computerChoice === 'scissors') {
+      playerScore += 0
+      computerScore += 0
       window.alert('Tie')
     } else {
       playerScore += 0
       computerScore += 1
       window.alert('Computer win the match because you wrong input.')
+    }
+
+    if (playerScore === 2 || computerScore === 2) {
+      break
     }
   }
 
@@ -70,5 +75,5 @@ if (decidedToCancel) {
     window.alert('You lose!, Computer get ' + computerScore + ' and you get ' + playerScore)
   }
 } else {
-  window.alert('You end the game.')
+  window.alert('Game end.')
 }
